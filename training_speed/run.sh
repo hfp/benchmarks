2350ece15b1a3450e8fab7f11fd74ba21726c08d
Hans Pabst <hans.pabst@intel.com>
2019-02-06 10:29:48 +0100

    Adjusted run-wrapper script.

--- training_speed/run.sh
+++ training_speed/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python run.py --iterations 400 --learners cat --experiment $1 --dataset-dir /panfs/projects/APAC_ML/ML_datasets/catboost-models
+python run.py --iterations 400 --learners cat --experiment $1 --dataset-dir ${HOME}/catboost-models
 
